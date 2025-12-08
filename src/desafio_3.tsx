b21bca580bfdf4fe4e54d4793af209c4b3b1f123
GustavoMaiaOliveira <gustavomaia334@gmail.com>
2025-12-08 10:24:55 -0300

    implement the useState on desafio_3

--- src/desafio_3.tsx
+++ src/desafio_3.tsx
@@ -1,7 +1,24 @@
-export function Calculadora () {
-    <div>
-        <p>
-            alo fml
-        </p>
-    </div>
+import { useState } from "react"
+
+export function Calculadora() {
+    const [number, setNumber] = useState("")
+
+    const handleChange = (event) => {
+        setNumber(event.target.value)
+    }
+
+    return (
+        <div className="Calculadora">
+            <p >Digite o primeiro número</p>
+            <input type="text" onChange={handleChange}/>
+            <p>{number}</p>
+            <button>+</button>
+            <button>-</button>
+            <button>/</button>
+            <button>*</button>
+            <p>Digite o segundo número</p>
+            <input type="text" onChange={handleChange}/>
+        </div>
+        
+    )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Contador } from './desafio_2'
 import { BoasVindas } from './desafio_1'
-// import { Calculadora } from './desafio_3'
+import { Calculadora } from './desafio_3'
 
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
-    <p className='flex justify-center'>Desafio 1</p><br />
+    <p>Desafio 1</p><br />
     <BoasVindas name='Maia' idade={20} />
     <br /><p>Desafio 2</p><br />
   <Contador/>
+  <br /><p>Desafio 3</p><br />
+  <Calculadora/>
   </StrictMode>,
 )
 
